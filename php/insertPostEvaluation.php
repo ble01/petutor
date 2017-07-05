e575147612dc5986b30cc3475b3c3895215009be
ble <blesmips@yahoo.com>
2017-07-05 11:12:36 +0100

    renamed table to post_evaluation

--- php/insertPostEvaluation.php
+++ php/insertPostEvaluation.php
@@ -9,7 +9,7 @@ $feedback = $_POST['feedback'];
 echo $id. " " .$user_id. " " .$feedback;
 //$con = mysqli_connect("localhost","root","","tutor");  //server,username,password,DB_name
 
-$sql = "INSERT INTO evaluation(id, user_id, query_id, feedback) VALUES('".$id."','".$user_id."','".$query_id."','".$feedback."')";
+$sql = "INSERT INTO post_evaluation(id, user_id, query_id, feedback) VALUES('".$id."','".$user_id."','".$query_id."','".$feedback."')";
 	
 mysqli_query($con, $sql);
 mysqli_close($con);
