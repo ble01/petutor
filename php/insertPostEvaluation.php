466c22c29038b83f3d8167e77a26ceb0f7914ac2
ble01 <bmbipom@gmail.com>
2017-07-06 12:57:31 +0100

    removed id

--- php/insertPostEvaluation.php
+++ php/insertPostEvaluation.php
@@ -1,7 +1,7 @@
 <?php
 include("dbConnect.php"); //Establishing connection with our database
 
-$id = $_POST['id'];
+// $id = $_POST['id'];
 $user_id = $_POST['user_id'];
 $query_id = $_POST['query_id'];
 $feedback = $_POST['feedback'];
@@ -9,7 +9,8 @@ $feedback = $_POST['feedback'];
 echo $id. " " .$user_id. " " .$feedback;
 //$con = mysqli_connect("localhost","root","","tutor");  //server,username,password,DB_name
 
-$sql = "INSERT INTO post_evaluation(id, user_id, query_id, feedback) VALUES('".$id."','".$user_id."','".$query_id."','".$feedback."')";
+// $sql = "INSERT INTO post_evaluation(id, user_id, query_id, feedback) VALUES('".$id."','".$user_id."','".$query_id."','".$feedback."')";
+$sql = "INSERT INTO post_evaluation(user_id, query_id, feedback) VALUES('".$user_id."','".$query_id."','".$feedback."')";
 	
 mysqli_query($con, $sql);
 mysqli_close($con);
