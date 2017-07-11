331a2edbe938c1e1c89c337969146ff7715b7f3b
ble <blesmips@yahoo.com>
2017-07-11 18:51:39 +0100

    included date

--- php/insertSurvey.php
+++ php/insertSurvey.php
@@ -8,17 +8,18 @@ $consent = $role = $qualification = $experience = $expertise = "";
 $ip = ""; 
 $user_id = $_POST['user_id'];
 $ip = $_SERVER["REMOTE_ADDR"];
+$date = gmdate("l jS \of F Y h:i:s A");
 $consent = $_POST['consent'];
 $role = $_POST['role'];
 $qualification = $_POST['qualification'];
 $experience = $_POST['experience'];
 $expertise = $_POST['expertise']; 
 
-echo $user_id. " " .$consent. " " .$role. " " .$qualification. " " .$experience. " " .$expertise;
+//echo $user_id. " " .$consent. " " .$role. " " .$qualification. " " .$experience. " " .$expertise;
 
  if(isset($_POST['consent'])){ 
 	 
-	 $sql = "INSERT INTO survey (user_id, ip, consent, role, qualification, experience, expertise) VALUES('".$user_id."','".$ip."','".$consent."','".$role."','".$qualification."','".$experience."','".$expertise."')"; 
+	 $sql = "INSERT INTO survey (user_id, ip, date, consent, role, qualification, experience, expertise) VALUES('".$user_id."','".$ip."','".$date."','".$consent."','".$role."','".$qualification."','".$experience."','".$expertise."')"; 
  
  } 
 
